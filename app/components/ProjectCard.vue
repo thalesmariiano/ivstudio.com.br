9286f026188440f29a04d844dd23bf649f869fbe
Thales Mariano <thales.mariano125@gmail.com>
2025-09-12 12:32:56 -0300

    feat: add projects slider

--- app/components/ProjectCard.vue
+++ app/components/ProjectCard.vue
@@ -1,17 +1,20 @@
+<script setup lang="ts">
+    const props = defineProps(['project']);
+</script>
+
 <template>
     <div class="w-[15rem] flex flex-col justify-start items-center bg-neutral-400 rounded-4xl overflow-hidden">
         <div class="w-full h-44 bg-neutral-500">
-
+            <img :src="'projects/'+ props.project.project_image + '.png'"
         </div>
         <div class="w-full h-48 p-[2.5px] bg-gradient-to-r from-neutral-900 to-neutral-700">
             <div class="w-full h-full flex flex-col justify-between items-start bg-neutral-800 rounded-b-[1.8rem] px-5 py-4">
                 <div class="space-y-2">
                     <p class="font-poppins font-semibold text-white text-lg">
-                        MenuTap - landi...
+                        {{ props.project.name }}
                     </p>
                     <p class="font-poppins font-light text-[11px] text-neutral-500 leading-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quaerat explicabo repellendus
+                        {{ props.project.description.slice(0, 150) + '...' }}
                     </p>
                 </div>
                 <div class="w-full flex justify-between items-center">
@@ -20,11 +23,14 @@
                         <span class="w-2 h-2 bg-green-500 rounded-full"></span>
 
                     </div>
-                    <a
-                        class="flex gap-1 items-center bg-gradient-to-r from-neutral-300 from-75% to-neutral-500 px-2.5 py-1 rounded-xl font-poppins font-semibold text-xs">
+                    <NuxtLink
+                        :to="props.project.project_link"
+                        target="_blank"
+                        class="flex gap-1 items-center bg-gradient-to-r from-neutral-300 from-75% to-neutral-500 px-2.5 py-1 rounded-xl font-poppins font-semibold text-xs"
+                    >
                         Acessar
                         <IconsArrowForward width="18" height="18" stroke="#000" />
-                    </a>
+                    </NuxtLink>
                 </div>
             </div>
         </div>
--- app/components/ProjectsSlider.vue
+++ app/components/ProjectsSlider.vue
@@ -1,6 +1,45 @@
+<script setup lang="ts">
+    import projects from '@/assets/data/projects.json';
+    import Splide from '@splidejs/splide'
+
+    const splide = ref('splide');
+
+	onMounted(() => {
+        nextTick(() => {
+            const slider =  new Splide(splide.value, {
+                perPage: 4,
+                perMove: 1,
+                arrows: false,
+                breakpoints: {
+                    1082: {
+                        perPage: 3
+                    },
+                    760: {
+                        perPage: 2
+                    },
+                    510: {
+                        perMove: 1
+                    }
+                }
+            })
+
+            slider.on( 'pagination:mounted', function ( data ) {
+                // You can add your class to the UL element
+                data.list.classList.add('translate-y-12');
+
+                // `items` contains all dot items
+                // data.items.forEach( function ( item ) {
+                //     item.button.textContent = String( item.page + 1 );
+                // } );
+            } );
+            slider.mount();
+        });
+    }) ;
+</script>
+
 <template>
     <div class="w-full py-16 bg-gradient-to-t from-black from-70%">
-        <div class="w-full max-w-4xl flex justify-between items-center mx-auto mb-10">
+        <div class="w-full max-w-4xl flex justify-between items-center px-3 mx-auto mb-10">
             <p class="font-poppins font-medium text-white italic">
                 <span class="text-neutral-500">//</span>
                 Confira alguns projetos
@@ -10,12 +49,22 @@
                 <IconsArrowForward width="18" height="18" stroke="#fff" />
             </a>
         </div>
-        <div class="w-full flex gap-5 justify-center items-center">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
+
+        <div class="w-full flex justify-center items-center">
+            <div ref="splide" class="splide w-full max-w-5xl">
+                <div class="splide__track">
+                    <ul class="splide__list">
+                        <li
+                            v-for="project in projects"
+                            :key="project.id"
+                            class="splide__slide flex justify-center"
+                        >
+                            <ProjectCard :project />
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
+        
     </div>
 </template>
