1b9326433fa54dec20ab489175137123f16c525d
Thales Mariano <thales.mariano125@gmail.com>
2026-03-19 14:14:02 -0300

    feat: update video poster image to a high quality one

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@
 
 <template>
     
-    <video class="w-full h-screen overflow-x-hidden object-cover absolute top-0 left-0 -z-5" poster="@/assets/images/video-poster.png" autoplay muted loop playsinline webkit-playsinline disablepictureinpicture preload="auto">
+    <video class="w-full h-screen overflow-x-hidden object-cover absolute top-0 left-0 -z-5" poster="@/assets/images/start-bg.png" autoplay muted loop playsinline webkit-playsinline disablepictureinpicture preload="auto">
         <source src="@/assets/videos/hero-video-4.mp4" type="video/mp4">
         <!-- <source src="@/assets/videos/hero-video.ogg" type="video/ogg"> -->
             Your browser does not support the video tag.
